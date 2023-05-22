0bcb5a15c73d12949ed72c76210be4174b21a68a
user-nameman-pr <contact@gmail.comdev.mprioux@gmail.com>
2023-05-22 16:31:39 +0200

    début main

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// MENUBURGER
+
+const hamburgerButton = document.querySelector(".nav-toggler")
+
+const navigation = document.querySelector ("nav")
+
+hamburgerButton.addEventListener("click",toggleNav)
+
+function toggleNav () {
+    hamburgerButton.classList.toggle ("active")
+    navigation.classList.toggle ("active")
+}
+
